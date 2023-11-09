acd2f4b409939e270be26cea6e0ae3c946d22eb5
SIMONE\simon <simonecampisi2003@gmail.com>
2023-11-09 12:03:40 +0100

    add component

--- ecommerce/src/app/app-routing.module.ts
+++ ecommerce/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProductViewComponent} from "./home/product-view/product-view.component";
+import {ProductComponent} from "./home/product-view/product/product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, children: [
+      { path: 'products', component: ProductViewComponent, children: [{
+        path: 'product', component: ProductComponent
+      }]}
+    ]},
+  // Altre route se necessario
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductViewComponent } from './home/product-view/product-view.component';
+import { ProductComponent } from './home/product-view/product/product.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductViewComponent,
+    ProductComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
