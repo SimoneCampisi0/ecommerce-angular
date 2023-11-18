14562b8bf39c67c70ecf6da37f01e40d63524661
SIMONE\simon <simonecampisi2003@gmail.com>
2023-11-18 15:37:01 +0100

    add product-page and hero component.

---
+++ ecommerce/src/app/product-page/product-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrl: './product-page.component.css'
+})
+export class ProductPageComponent {
+
+  ngOnInit() {
+    console.log("Product-page")
+  }
+
+}
