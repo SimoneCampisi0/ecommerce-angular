0e8fb296e346ca5d06d9fa655208f99c453a62bc
SIMONE\simon <simonecampisi2003@gmail.com>
2024-01-11 11:17:05 +0100

    vedi TODO

--- ecommerce/src/app/product-module/product-detail/product-detail.component.ts
+++ ecommerce/src/app/product-module/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-product-module-detail',
@@ -7,10 +7,18 @@ import {ActivatedRoute} from "@angular/router";
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent implements OnInit {
+  id: number = 0
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() { //TODO: Da product.component.ts non si riesce ad arrivare qui.
     console.log("Product detail.")
-    const id = Number(this.route.snapshot.params['id'])
+    // const id = Number(this.route.snapshot.params['id'])
+
+    this.route.params //Asincrono, si preferisce.
+      .subscribe(
+        (params: Params) => {
+          this.id = Number(params['id'])
+        }
+      )
   }
 }
