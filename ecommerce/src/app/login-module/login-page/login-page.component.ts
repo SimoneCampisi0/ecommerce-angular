7f6ebaa23fbdf243e417d8cd91289bf1e9003d55
SIMONE\simon <simonecampisi2003@gmail.com>
2024-01-16 15:12:25 +0100

    fix login page.

--- ecommerce/src/app/login-module/login-page/login-page.component.ts
+++ ecommerce/src/app/login-module/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../../../services/auth.service";
 import {Router} from "@angular/router";
 import {LoginUserRequest} from "../../../dtos/LoginUserRequest";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-login-page',
@@ -11,14 +12,39 @@ import {LoginUserRequest} from "../../../dtos/LoginUserRequest";
 })
 export class LoginPageComponent {
   loginForm = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl(),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
   })
 
   constructor(private router: Router, private authService: AuthService) {}
 
+  checkFormWithoutEmailAndPassword() {
+    for(let controlName in this.loginForm.controls) {
+      if(this.loginForm.controls.hasOwnProperty(controlName)) {
+        if(!this.loginForm.get(controlName)?.valid) { //Se esiste almeno un campo non valido, ritorna false
+          return false;
+        }
+      }
+    }
+    return true; //Altrimenti ritorna true
+  }
+
+  checkLogin() {
+    console.log("check: "+this.checkFormWithoutEmailAndPassword())
+
+    if(!this.checkFormWithoutEmailAndPassword()) {
+      Swal.fire({
+        icon: "error",
+        title: "Dati non validi",
+        text: "Inserisci correttamente i dati."
+      });
+    } else {
+        this.onLogin()
+    }
+  }
+
   onLogin() {
-    let request = new LoginUserRequest(this.loginForm.value.email, this.loginForm.value.password)
+    let request = new LoginUserRequest(this.loginForm.value.email || "", this.loginForm.value.password || "")
 
     this.authService.login(request).subscribe({
       next: (response) => {
