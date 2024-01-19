d52c06811adf8b7930493fe713b72bf108088254
SIMONE\simon <simonecampisi2003@gmail.com>
2024-01-19 15:51:34 +0100

    change product css.

--- ecommerce/src/app/product-module/product-view/product/product.component.ts
+++ ecommerce/src/app/product-module/product-view/product/product.component.ts
@@ -11,23 +11,33 @@ export class ProductComponent implements OnInit {
   @Input() idProduct = 0
   @Input() titolo = ""
   @Input() avaiable = false
-  @Input() costo = 0
+  @Input() costoTotale = 0
+  costoIntero: number = 0
+  costoDecimale = 0
 
   // @Input() imgURL = ""
 
   constructor(private router: Router, private route: ActivatedRoute, private sharedService: SharedService) {}
 
   ngOnInit() {
+    this.setDecimalPart()
   }
 
+  setDecimalPart() {
+    if(Number.isInteger(this.costoTotale)) {
+      this.costoIntero = this.costoTotale
+      this.costoDecimale = 0
+    } else {
+      this.costoDecimale = Number(this.costoTotale.toString().split('.')[1])
+      this.costoIntero = this.costoTotale - this.costoDecimale
+    }
+  }
 
   goProductDetails() {
     this.sharedService.changeSelectedProduct(true)
 
     this.router.navigate(
-      // ['/home/product', this.idProduct]
       ['../', 'home', 'product', this.idProduct], {relativeTo: this.route}
-      // { queryParams: { id: this.idProduct}}
     )
   }
 }
